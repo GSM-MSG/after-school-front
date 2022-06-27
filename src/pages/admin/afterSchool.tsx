b846267b2fa6c34e055e082b333300d8c854b41d
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-06-27 22:55:51 +0900

    Update graph page api 연동

--- src/pages/admin/afterSchool.tsx
+++ src/pages/admin/afterSchool.tsx
@@ -3,15 +3,31 @@ import { useState } from "react";
 import AdminAfterSchool from "../../components/AdminAfterSchool";
 import { CreateAfterSchool } from "../../components/CreateAfterSchool";
 import Header from "../../components/Header";
-import { list } from "../../components/AdminAfterSchool/dummyData";
 import { PropListType } from "../../types";
+import userCheck from "../../lib/userCheck";
+import api from "../../lib/api";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  return {
-    props: {
-      data: list,
-    },
-  };
+  try {
+    const { cookies, accessToken } = await userCheck(ctx);
+
+    const { data } = await api.get("/afterschool/statistics", {
+      headers: { cookies: `accessToken=${accessToken}` },
+    });
+
+    if (cookies) ctx.res.setHeader("set-cookie", cookies);
+
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (e) {
+    return {
+      props: {},
+      redirect: { destination: "/login" },
+    };
+  }
 };
 
 interface AfterSchoolProps {
