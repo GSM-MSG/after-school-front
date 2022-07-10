ce4370b93d75b9188e8b07c6c7e27386bca7c9eb
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-07-10 13:20:30 +0900

    Update admin login page

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,29 @@
+import { GetServerSideProps } from "next";
 import AdminLogin from "../../components/AdminLogin";
 import SEO from "../../components/SEO";
+import admin from "../../lib/admin";
+import userCheck from "../../lib/userCheck";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  try {
+    const { cookies, accessToken } = await userCheck(ctx);
+
+    await admin.get("/afterschool", {
+      headers: { cookie: `adminAccessToken=${accessToken}` },
+    });
+
+    if (cookies) ctx.res.setHeader("set-cookie", cookies);
+
+    return {
+      props: {},
+      redirect: { destination: "/admin/afterSchool" },
+    };
+  } catch (e) {
+    return {
+      props: {},
+    };
+  }
+};
 
 export default function AdminPage() {
   return (
